569d7112c4767eeb52881a840c3b437f27f60220
lyx <canoecanoe@aliyun.com>
2021-04-10 17:13:00 +0800

    project init

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,69 @@
+// miniprogram/pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgList: [
+      "https://s.cn.bing.net/th?id=ODL.b9006520ab53aa98b9717a8328011606&w=197&h=112&c=7&rs=1&qlt=80&dpr=1.5&pid=RichNav",
+      "https://s.cn.bing.net/th?id=ODL.b9006520ab53aa98b9717a8328011606&w=197&h=112&c=7&rs=1&qlt=80&dpr=1.5&pid=RichNav"
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})

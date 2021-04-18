8ff417b90c834ff43b33995e9078812a0ab6f696
lyx <canoecanoe@aliyun.com>
2021-04-18 17:03:45 +0800

    完成login云函数的自动登录

--- miniprogram/pages/user/user.js
+++ miniprogram/pages/user/user.js
@@ -1,5 +1,5 @@
 // miniprogram/pages/user/user.js
-
+const app = getApp()
 const db = wx.cloud.database()
 
 Page({
@@ -10,19 +10,46 @@ Page({
   data: {
     userPhoto: "../../images/tabbar/user_photo.png",
     nickName: "aa",
-    isLogin: false
-  },
+    isLogin: false,
+    disabled: false
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  },
   onLoad: function (options) {
 
   },
-  bindGetUserInfo (ev) {
-    let {userInfo} = ev.detail
+  // 当页面渲染完，登录过的用户自动登录
+  onReady() {
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {}
+    }).then(res => {
+      // 查询
+      db.collection('users').where({
+        _openid: res.result.openid
+      }).get().then(res => {
+        if (res.data.length) {
+          // 将res.data拷贝到app.userInfo
+          app.userInfo = Object.assign(app.userInfo, res.data[0])
+          this.setData({
+            userPhoto: app.userInfo.userPhoto,
+            nickName: app.userInfo.nickName,
+            isLogin: true,
+          })
+        }
+        else {
+          this.setData({
+            disabled: false
+          })
+        }
+      })
+    })
+  },
+  bindGetUserInfo(ev) {
+    let {
+      userInfo
+    } = ev.detail
     console.log(userInfo);
-    if (!this.data.isLogin &&  userInfo) {
+    if (!this.data.isLogin && userInfo) {
       db.collection('users').add({
         data: {
           userPhoto: userInfo.avatarUrl,
@@ -37,7 +64,17 @@ Page({
           time: new Date()
         }
       }).then(res => {
-        console.log(res)
+        // 将用户信息写入全局
+        db.collection('users').doc(res._id).get().then(res => {
+          // 用于将所有可枚举属性的值从一个或多个源对象复制到目标对象
+          // 将res.data拷贝到app.userInfo
+          app.userInfo = Object.assign(app.userInfo, res.data)
+          this.setData({
+            userPhoto: app.userInfo.userPhoto,
+            nickName: app.userInfo.nickName,
+            isLogin: true
+          })
+        })
       })
     }
   }

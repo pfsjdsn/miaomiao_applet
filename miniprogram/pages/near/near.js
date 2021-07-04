57ae2078c783ec4ec63f01e4990887c1dfeb7500
lyx <canoecanoe@aliyun.com>
2021-07-04 18:15:44 +0800

    跳转附近详情页(未完成)

--- miniprogram/pages/near/near.js
+++ miniprogram/pages/near/near.js
@@ -14,7 +14,8 @@ Page({
     // 纬度
     latitude: "",
     // 标记点
-    markers: []
+    markers: [],
+    map: 1,
   },
 
   /**
@@ -28,6 +29,8 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    // this.mapCtx = wx.createMapContext('myMap')
+    console.log(this.mapCtx, 'this.mapCtx');
     this.getLocation()
   },
 
@@ -54,12 +57,14 @@ Page({
   },
   // 获取附近的人位置
   getNearUser() {
+    console.log(this.data.longitude);
+    console.log(this.data.latitude);
     db.collection('users').where({
       location: _.geoNear({
         geometry: db.Geo.Point(this.data.longitude, this.data.latitude),
         // 最大距离与最小距离，单位为米
         minDistance: 0,
-        maxDistance: 100000
+        maxDistance: 100000000
       }),
       // 当用户开启位置共享时，才可获取附近人的信息
       isLocation: true
@@ -68,25 +73,57 @@ Page({
       longitude: true,
       userPhoto: true
     }).get().then((res) => {
+      console.log(res, );
       let data = res.data
       let result = []
       if (data.length) {
         for (let i = 0; i < data.length; i++) {
-          result.push({
-            // iconPath: "../../images/tabbar/msg.png",
-            iconPath: data[i].userPhoto,
-            // id: data[i]._id,
-            id: 0,
-            latitude: data[i].latitude,
-            longitude: data[i].longitude,
-            width: 30,
-            height: 30
-          })
+          if (data[i].userPhoto.includes('cloud://')) {
+            wx.cloud.getTempFileURL({
+              fileList: [data[i].userPhoto],
+              success: res => {
+                console.log(res, '1111111111')
+                result.push({
+                  iconPath: data[i].userPhoto,
+                  id: data[i]._id,
+                  // id: 1,
+                  latitude: data[i].latitude,
+                  longitude: data[i].longitude,
+                  width: 30,
+                  height: 30
+                })
+                console.log(111111111);
+                this.setData({
+                  markers: result
+                })
+                console.log(this.data.markers);
+              },
+            })
+          } else {
+            console.log('我来这里了');
+            result.push({
+              iconPath: data[i].userPhoto,
+              // id: 1,
+              id: data[i]._id,
+              latitude: data[i].latitude,
+              longitude: data[i].longitude,
+              width: 30,
+              height: 30
+            })
+          }
         }
         this.setData({
           markers: result
         })
+        console.log(this.data.markers);
       }
+    })
+  },
+  // 点击标记点时触发
+  markertap(ev) {
+    // console.log(ev);
+    wx.navigateTo({
+      url: '/pages/detail/detail?userId=' + ev.target.dataset.myid,
     })
   }
 })

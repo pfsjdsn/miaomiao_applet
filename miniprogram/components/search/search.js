76d0c474d53228358220fb102375f95bdb3ae531
lyx <canoecanoe@aliyun.com>
2021-07-11 17:12:07 +0800

    实现搜索历史记录及本地缓存(完成删除缓存 功能)

--- miniprogram/components/search/search.js
+++ miniprogram/components/search/search.js
@@ -13,13 +13,53 @@ Component({
    * 组件的初始数据
    */
   data: {
-    isFocus: true
+    isFocus: false,
+    historyList: []
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    // 聚焦事件
+    handleFocus() {
+      wx.getStorage({
+        key: 'searchHistory',
+        success: (res) => {
+          this.setData({
+            historyList: res.data
+          })
+        }
+      })
+      this.setData({
+        isFocus: true
+      })
+    },
+    // 取消事件
+    handleCancel() {
+      this.setData({
+        isFocus: false
+      })
+    },
+    // 回车事件
+    handleConfirm(ev) {
+      let cloneHistoryList = [...this.data.historyList]
+      cloneHistoryList.unshift(ev.detail.value)
+      wx.setStorage({
+        key: "searchHistory",
+        data: [...new Set(cloneHistoryList)]
+      })
+    },
+    // 删除
+    handleDelete() {
+      wx.removeStorage({
+        key: 'searchHistory',
+        success: (res) => {
+          this.setData({
+            historyList: []
+          })
+        }
+      })
+    }
   }
 })
